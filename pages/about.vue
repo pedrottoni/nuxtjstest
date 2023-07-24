68cb4a8631610fec7b261dd20195de3cf33318b8
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2023-07-24 17:20:24 -0300

    criando o menu

---
+++ pages/about.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  // definePageMeta({
+  // 	layout: 'default',
+  // 	name: 'about',
+  // 	title: 'About',
+  //	navOrder: 4,
+  // })
+</script>
+
+<template>
+  <div
+    style="
+      min-height: 100px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: bold;
+    "
+  >
+    <span style="color: #0198cd">about.vue (Nuxt 3.6)</span>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/team.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+	// definePageMeta({
+	// 	layout: 'default',
+	// 	title: 'Team',
+	// })
+</script>
+
+<template>
+	<div style="min-height: 100px; display: flex; align-items: center; justify-content: center; font-weight: bold">
+		<span style="color: #0198cd">team.vue (Nuxt 3.6)</span>
+	</div>
+</template>
+
+<style scoped></style>
+
