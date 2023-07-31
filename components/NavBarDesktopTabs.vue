75910ceea8cae483019754cfe30a3c9b1fe87039
Pedro Ottoni Carneiro <pedrottoni@gmail.com>
2023-07-31 15:55:49 -0300

    ícones e svg

--- components/NavBarDesktopTabs.vue
+++ components/NavBarDesktopTabs.vue
@@ -15,9 +15,17 @@
   })
 </script>
 <template>
-    <div class="h-full items-center flex space-x-3">
-        <UButton v-for="(navlink, index) in navlinks" :key="index" :to="navlink.link" :label="navlink.text" size="lg" variant="ghost" :color="isCurrentRoute(navlink, currentPath) ? 'primary' : 'blue'">
-</UButton>
-    </div>
+  <div class="h-full items-center flex space-x-0">
+    <UButton
+      v-for="(navlink, index) in navlinks"
+      :key="index"
+      :to="navlink.link"
+      :label="navlink.text"
+      size="lg"
+      variant="ghost"
+      :color="isCurrentRoute(navlink, currentPath) ? 'primary' : 'blue'"
+    >
+    </UButton>
+  </div>
 </template>
 <style scoped></style>
